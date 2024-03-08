3f7bdabe89c105cc4c9dc8b6fbe9d039c71b0193
AmeyKuradeAK <ameykurade60@gmail.com>
2024-03-08 22:53:45 +0530

    Blender Sactter Addon Created

---
+++ blender_scatter.py
@@ -0,0 +1,7 @@
+bl_info = {
+    "name": "Blender Scatter",
+    "author": "Amey Kurade",
+    "version": (1, 0, 0),
+    "blender": (4, 0, 2),  # Adjust based on Blender version you target
+    "category": "Object"
+}
